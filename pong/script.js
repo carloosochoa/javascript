3cb8f2f2c793c3640e199c23e4e440f61955cf94
Carlos Ochoa <carlosochoala03@gmail.com>
2023-11-13 17:03:04 +0100

    pong y tareas

--- pong/script.js
+++ pong/script.js
@@ -1,3 +1,29 @@
 const paddle = document.getElementById("paddle");
 const ball = document.getElementById("ball");
 
+const radio_balon = 20;
+
+var velocidadX = 15; //pixeles
+var velocidadY = 5;
+intervalo = false;
+alturaBody = window.innerHeight;
+console.log(alturaBody);
+
+
+document.addEventListener('keydown',function(event){
+    if (!intervalo) {
+        intervalo = setInterval(() =>{
+            if(event.key === "w"){
+                paddle.style.top = (paddle.offsetTop - velocidadY) + "px";
+            } else if(event.key === "s") {
+                paddle.style.top = (paddle.offsetTop + velocidadY) + "px";
+            }
+            },10);
+    }
+});
+
+document.addEventListener('keyup',function(){
+    clearInterval(intervalo);
+    intervalo = false;
+});
+
